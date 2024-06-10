1a88c522454eec8c66759447662a1bda1e61e250
dongmin30 <dongmin0355@gmail.com>
2024-06-10 14:35:19 +0900

    loading component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,29 @@
-"use client"
+import Link from "next/link";
 
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home",
+};
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  }
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovies() {
+  // URL을 통해 fetch된 내용을 nextjs에서 캐싱해준다.
+  // but 실시간 데이터를 가져오는데에는 변경점이 생길 수 있다.
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }

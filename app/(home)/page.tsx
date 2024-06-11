629913317cccab28df04aa2f2838c32b95fac501
dongmin30 <dongmin0355@gmail.com>
2024-06-11 09:20:25 +0900

    Suspens, Error Handling

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,13 +4,13 @@ export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   // URL을 통해 fetch된 내용을 nextjs에서 캐싱해준다.
   // but 실시간 데이터를 가져오는데에는 변경점이 생길 수 있다.
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
